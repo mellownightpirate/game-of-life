cae7f52d14bf3afb7a5405e549c477092d42c02e
mellownightpirate <a@hasan.co>
2020-06-23 11:52:19 +0100

    add play button, add rules logic

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 class Box extends React.Component {
   selectBox = () => {
     this.props.selectBox(this.props.row, this.props.col);
-  };
+  }
   render() {
     return (
       <div
@@ -18,7 +18,7 @@ class Box extends React.Component {
 }
 class Grid extends React.Component {
   render() {
-    const width = this.props.cols * 16;
+    const width = (this.props.cols * 16);
     var rowsArr = [];
 
     var boxClass = "";
@@ -83,9 +83,41 @@ class Main extends React.Component {
     });
   };
 
-  componentDidMount() {
-    this.seed();
+  playButton = () => {
+    clearInterval(this.intervalId)
+    this.intervalId = setInterval(this.play, this.speed)
   }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
+
+  }
+	componentDidMount() {
+		this.seed();
+		this.playButton();
+      }
+      
   render() {
     return (
       <div>
